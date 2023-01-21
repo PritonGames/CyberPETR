6c06a935e9444197b64829f72e946739ed51046f
Влад Апрелков <111743049+vladysanys@users.noreply.github.com>
2023-01-21 13:44:35 +0700

    Add files via upload

--- js/gravity.js
+++ js/gravity.js
@@ -1,154 +1,131 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-canvas.height = 600;
-canvas.width = 1000;
-
 var player = {
   x: 200,
   y: 200,
   x_v: 0,
   y_v: 0,
-  jump: true,
-  height: 60,
-  width: 40,
-};
+  jump : true,
+  height: 20,
+  width: 20
+  };
 // The status of the arrow keys
 var keys = {
   right: false,
   left: false,
   up: false,
-};
-// The friction and gravity to show realistic movements
-var gravity = 0.3;
+  };
+// The friction and gravity to show realistic movements    
+var gravity = 0.6;
 var friction = 0.7;
 // The number of platforms
 var num = 2;
 // The platforms
 var platforms = [];
 // Function to render the canvas
-function rendercanvas() {
+function rendercanvas(){
   ctx.fillStyle = "#F0F8FF";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, 270, 270);
 }
 // Function to render the player
-function renderplayer() {
+function renderplayer(){
   ctx.fillStyle = "#F08080";
-  ctx.fillRect(player.x - 40, player.y - 60, player.width, player.height);
-}
+  ctx.fillRect((player.x)-20, (player.y)-20, player.width, player.height);
+  }
 // Function to create platforms
-function createplat() {
-    platforms.push({
-      x: 0,
-      y: canvas.height - 40,
-      width: canvas.width,
-      height: canvas.height,
-    });
-    platforms.push({
-        x: 600,
-        y: 390,
-        width: 250,
-        height: 35,
-      });
-    console.log(platforms)
-}
+function createplat(){
+  for(i = 0; i < num; i++) {
+      platforms.push(
+          {
+          x: 100 * i,
+          y: 200 + (30 * i),
+          width: 110,
+          height: 15
+          }
+      );
+  }
+  }
 // Function to render platforms
-function renderplat() {
+function renderplat(){
   ctx.fillStyle = "#45597E";
-  ctx.fillRect(
-    platforms[0].x,
-    platforms[0].y,
-    platforms[0].width,
-    platforms[0].height
-  );
-  ctx.fillRect(
-    platforms[1].x,
-    platforms[1].y,
-    platforms[1].width,
-    platforms[1].height
-  );
+  ctx.fillRect(platforms[0].x, platforms[0].y, platforms[0].width, platforms[0].height);
+  ctx.fillRect(platforms[1].x, platforms[1].y, platforms[1].width,platforms[1]. height);
+
 }
 // This function will be called when a key on the keyboard is pressed
 function keydown(e) {
   // 37 is the code for the left arrow key
-  if (e.keyCode == 37) {
-    keys.left = true;
+  if(e.keyCode == 37) {
+      keys.left = true;
   }
   // 37 is the code for the up arrow key
-  if (e.keyCode == 38) {
-    if (player.jump == false) {
-      player.y_v = -10;
-    }
+  if(e.keyCode == 38) {
+      if(player.jump == false) {
+          player.y_v = -10;
+      }
   }
   // 39 is the code for the right arrow key
-  if (e.keyCode == 39) {
-    keys.right = true;
+  if(e.keyCode == 39) {
+      keys.right = true;
   }
 }
 // This function is called when the pressed key is released
 function keyup(e) {
-  if (e.keyCode == 37) {
-    keys.left = false;
+  if(e.keyCode == 37) {
+      keys.left = false;
   }
-  if (e.keyCode == 38) {
-    if (player.y_v < -2) {
+  if(e.keyCode == 38) {
+      if(player.y_v < -2) {
       player.y_v = -3;
-    }
+      }
   }
-  if (e.keyCode == 39) {
-    keys.right = false;
+  if(e.keyCode == 39) {
+      keys.right = false;
   }
-}
+} 
 function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
   // If the player is not jumping apply the effect of frictiom
-  if (player.jump == false) {
-    player.x_v *= friction;
+  if(player.jump == false) {
+      player.x_v *= friction;
   } else {
-    // If the player is in the air then apply the effect of gravity
-    player.y_v += gravity;
+      // If the player is in the air then apply the effect of gravity
+      player.y_v += gravity;
   }
   player.jump = true;
   // If the left key is pressed increase the relevant horizontal velocity
-  if (keys.left) {
-    player.x_v = -5.5;
+  if(keys.left) {
+      player.x_v = -2.5;
   }
-  if (keys.right) {
-    player.x_v = 5.5;
+  if(keys.right) {
+      player.x_v = 2.5;
   }
   // Updating the y and x coordinates of the player
   player.y += player.y_v;
   player.x += player.x_v;
   // A simple code that checks for collions with the platform
   let i = -1;
-  if (
-    platforms[0].x < player.x &&
-    player.x < platforms[0].x + platforms[0].width &&
-    platforms[0].y < player.y &&
-    player.y < platforms[0].y + platforms[0].height
-  ) {
-    i = 0;
+  if(platforms[0].x < player.x && player.x < platforms[0].x + platforms[0].width &&
+  platforms[0].y < player.y && player.y < platforms[0].y + platforms[0].height){
+      i = 0;
   }
-  if (
-    platforms[1].x < player.x &&
-    player.x < platforms[1].x + platforms[1].width &&
-    platforms[1].y < player.y &&
-    player.y < platforms[1].y + platforms[1].height
-  ) {
-    i = 1;
+  if(platforms[1].x < player.x && player.x < platforms[1].x + platforms[1].width &&
+  platforms[1].y < player.y && player.y < platforms[1].y + platforms[1].height){
+      i = 1;
   }
-  if (i > -1) {
-    player.jump = false;
-    player.y = platforms[i].y;
+  if (i > -1){
+      player.jump = false;
+      player.y = platforms[i].y;    
   }
   // Rendering the canvas, the player and the platforms
   rendercanvas();
   renderplayer();
   renderplat();
-  // console.log(1)
 }
+canvas=document.getElementById("canvas");
+ctx=canvas.getContext("2d");
+ctx.canvas.height = 270;
+ctx.canvas.width = 270;
 createplat();
 // Adding the event listeners
-document.addEventListener("keydown", keydown);
-document.addEventListener("keyup", keyup);
-setInterval(loop, 22);
+document.addEventListener("keydown",keydown);
+document.addEventListener("keyup",keyup);
+setInterval(loop,22);
+&& platforms[0].y < player.y && player.y < platforms[0].y + platforms[0].height
--- js/index-2.js
+++ js/index-2.js
@@ -23,12 +23,34 @@ let jump = {
   length: 50,
   height: 0,
 };
+let num = 1;
+let sum = 0;
 let deltaTime = 0;
 let lastUpdate = 0;
 let maxInteval = 40;
-let backgroundSpeed = 0
-let backgroundBoolean = false
-let backgroundCounter = 400
+let backgroundSpeed = 0;
+let backgroundBoolean = false;
+let backgroundCounter = 400;
+let platforms = [];
+function createplat() {
+  for (i = 0; i < num; i++) {
+    platforms.push({
+      x: 500,
+      y: 300,
+      width: 160,
+      height: 30,
+    });
+  }
+}
+function renderplat() {
+  context.fillStyle = "#45597E";
+  context.fillRect(
+    platforms[0].x,
+    platforms[0].y,
+    platforms[0].width,
+    platforms[0].height
+  );
+}
 function keydown(e) {
   const { key } = e;
   if (key == "ArrowRight" || e.keyCode == "68") {
@@ -39,6 +61,7 @@ function keydown(e) {
   }
   if (key == "ArrowUp" || e.keyCode == "32" || e.keyCode == "87dddd") {
     player.jump = true;
+    keys.up = true
   }
 }
 function keyup(e) {
@@ -50,14 +73,11 @@ function keyup(e) {
     keys.left = false;
   }
 }
-function hitbox(params) {}
 function animation(currentTime = 0) {
   // frameAnimation
   requestAnimationFrame(animation);
   deltaTime = currentTime - lastUpdate;
   correction = deltaTime / 1000;
-  let fps = 1000 / deltaTime;
-  
   //clear
   context.clearRect(0, 0, canvas.width, canvas.height);
   // background
@@ -69,13 +89,15 @@ function animation(currentTime = 0) {
   context.moveTo(0, 450);
   context.lineTo(canvas.width, 450);
   context.stroke();
-  context.beginPath();
-  context.moveTo(400, 0);
-  context.lineTo(400, 800);
-  context.stroke();
+
+  // context.stroke();
+  // context.beginPath();
+  // context.rect(500, 260, 200, 100);
+  // context.stroke();
   // player
+  let sum2 = player.y - jump.height;
   context.beginPath();
-  context.rect(player.x, player.y - jump.height, player.width, player.height);
+  context.rect(player.x, sum2, player.width, player.height);
   context.stroke();
   context.closePath();
   // animUpdate
@@ -97,29 +119,59 @@ function animation(currentTime = 0) {
       player.jump = false;
       jump.height = 0;
     }
-    if (player.x < backgroundSpeed *(-1) - 2) {
+    if (player.x < backgroundSpeed * -1 - 2) {
       player.x += player.x_v * correction;
     }
-    if(player.x + player.width > canvas.width + 2){
+    if (player.x + player.width > canvas.width + 2) {
       player.x -= player.x_v * correction;
     }
-    if(player.x > backgroundCounter && backgroundSpeed > -1900){
-      backgroundBoolean = true
+    if (player.x > backgroundCounter && backgroundSpeed > -1900) {
+      backgroundBoolean = true;
     }
     if (backgroundBoolean == true) {
-      backgroundSpeed -= player.x_v * correction
-      backgroundCounter += player.x_v * correction
+      backgroundSpeed -= player.x_v * correction;
+      backgroundCounter += player.x_v * correction;
       canvasBox.style = `left: ${backgroundSpeed}px`;
     }
-    if(player.x < backgroundCounter){
-      backgroundBoolean = false
+    if (player.x < backgroundCounter) {
+      backgroundBoolean = false;
     }
-
+    let i = -1;
+    // console.log(sum2)
+    if (
+      platforms[0].x < player.x &&
+      player.x < platforms[0].x + platforms[0].width &&
+      platforms[0].y < sum2 &&
+      sum2 < platforms[0].y + 10
+    ) {
+      i = 1;
+    } else {
+      i = -1
+    }
+    if(sum2 > 330 && player.x > platforms[0].x && player.x < platforms[0].x + platforms[0].height + platforms[0].width){
+      i = -1
+      jump.length = 18
+    }
+    if (i == 1) {
+      player.y = platforms[0].y - player.height
+      jump.length = 20
+      player.jump = false
+    } else if (i == -1 && player.x > platforms[0].x + platforms[0].width || player.x < 500 && player.x + player.width < 500){
+      player.y = 400
+      jump.length = 50
+    }
+    // if (player.x >= 500 && player.x <= 700 && jump.count > 49) {
+    //   player.y = 280 - player.height;
+    // } else {
+    // }
+    // console.log(jump.height)
   }
-  console.clear();
-  console.log("fps:", fps);
+  renderplat();
+
   lastUpdate = currentTime;
 }
+createplat();
 animation();
+console.log(platforms);
 document.addEventListener("keydown", keydown);
 document.addEventListener("keyup", keyup);
